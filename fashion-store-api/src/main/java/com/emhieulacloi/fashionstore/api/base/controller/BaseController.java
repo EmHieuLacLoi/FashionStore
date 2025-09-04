4ec1d7e4271b60823be95f5a81b4c4eaa392782f
Võ Trung Hiếu <86762037+EmHieuLacLoi@users.noreply.github.com>
2025-09-05 00:10:30 +0700

    fix: base controller

--- fashion-store-api/src/main/java/com/emhieulacloi/fashionstore/api/base/controller/BaseController.java
+++ fashion-store-api/src/main/java/com/emhieulacloi/fashionstore/api/base/controller/BaseController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 public class BaseController<IResponse, ICriteria, R, D, ID> {
 
     protected CustomQueryService<IResponse, ICriteria, ID> queryService;
