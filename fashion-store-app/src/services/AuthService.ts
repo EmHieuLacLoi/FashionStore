e6656f317c1a4787a4adef047278092c3bbf6314
Võ Trung Hiếu <86762037+EmHieuLacLoi@users.noreply.github.com>
2025-09-21 15:02:43 +0700

    fix call api

--- fashion-store-app/src/services/AuthService.ts
+++ fashion-store-app/src/services/AuthService.ts
@@ -1,10 +1,10 @@
 import axiosInstance from "@services/AxiosInstance";
 export const login = async (data: { username: string; password: string }) => {
-  const response = await axiosInstance.post("v1/auth/login", data);
+  const response = await axiosInstance.post("api/v1/auth/login", data);
   return response.data;
 };
 
 export const getUserInfo = async () => {
-  const response = await axiosInstance.get("v1/auth/info");
+  const response = await axiosInstance.get("api/v1/auth/info");
   return response.data;
 };
